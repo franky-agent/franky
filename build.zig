1a8fd26bda5ffad050ef9bcbd2a3e768f444bba1
fr12k <frank.ittermann@yahoo.de>
2026-05-06 10:25:27 +0200

    fix(cpu): reduce cpu usage dramatically by using libc

--- build.zig
+++ build.zig
@@ -23,6 +23,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
     const exe_module = b.createModule(.{
         .root_source_file = b.path("src/bin/main.zig"),
